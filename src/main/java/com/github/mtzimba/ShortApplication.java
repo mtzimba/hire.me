7e0f2936448c57d4a640ffaf5500725093997091
Matheus Cardoso <mtzimba@gmail.com>
2021-06-12 09:16:26 -0300

    Configuração inicial do projeto

---
+++ src/main/java/com/github/mtzimba/ShortApplication.java
@@ -0,0 +1,13 @@
+package com.github.mtzimba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortApplication.class, args);
+	}
+
+}
