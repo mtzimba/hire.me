63e66dc391b97c205b8b0ca2e222cb7b8022f39e
Matheus Cardoso <mtzimba@gmail.com>
2021-06-13 15:58:59 -0300

    criaado estrutura de pacote par api e client

---
+++ urlshortener-client/src/main/java/com/github/mtzimba/UrlshortenerClientApplication.java
@@ -0,0 +1,13 @@
+package com.github.mtzimba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlshortenerClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlshortenerClientApplication.class, args);
+	}
+
+}
